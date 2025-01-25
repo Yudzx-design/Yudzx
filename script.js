7bb9db4c9340b42325fbb0b219c30215f80a6632
Yudzx-design <anantdewa304@gmail.com>
2025-01-25 22:10:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Fungsi untuk login
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "Yudzx" && password === "admin1") {
+        document.getElementById("login-container").style.display = "none";
+        document.getElementById("video-container").style.display = "flex";
+
+        // Masuk ke mode fullscreen
+        const video = document.getElementById("video-player");
+        video.requestFullscreen()
+            .then(() => {
+                video.play();
+            })
+            .catch((err) => {
+                console.log("Fullscreen gagal: ", err.message);
+            });
+    } else {
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "Username atau Password salah!";
+    }
+}
+
+// Menampilkan atau menyembunyikan password
+document.getElementById("show-password").addEventListener("change", function () {
+    const passwordField = document.getElementById("password");
+    passwordField.type = this.checked ? "text" : "password";
+});
