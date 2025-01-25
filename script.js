5368a85e182591a6df280b6baaa1c3d03832ff57
Yudzx-design <anantdewa304@gmail.com>
2025-01-25 21:47:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Fungsi untuk login
 function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -5,6 +6,16 @@ function login() {
     if (username === "Yudzx" && password === "admin1") {
         document.getElementById("login-container").style.display = "none";
         document.getElementById("video-container").style.display = "flex";
+
+        // Masuk ke mode fullscreen
+        const video = document.getElementById("video-player");
+        video.requestFullscreen()
+            .then(() => {
+                video.play();
+            })
+            .catch((err) => {
+                console.log("Fullscreen gagal: ", err.message);
+            });
     } else {
         document.getElementById("error-message").textContent =
             "Username atau Password salah!";
